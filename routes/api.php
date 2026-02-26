f62a89b3a4f6338cd70f716176ad2d8704d2dbe6
imamsalji <imamsalji7@gmail.com>
2026-02-26 16:07:13 +0700

    Update Api Route

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\Transaction\CartController;
 use App\Http\Controllers\Api\Transaction\OrderController;
 use App\Http\Controllers\Api\Admin\OrderController as AdminOrderController;
+use App\Http\Controllers\Api\Admin\GameController as AdminGameController;
+use App\Http\Controllers\Api\Admin\ProductController as AdminProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -60,18 +62,18 @@ Route::prefix('v1/admin')->middleware(['auth:sanctum', 'admin'])->group(function
     Route::put('/orders/{id}/verify', [AdminOrderController::class, 'verify']);
 
     // GAME
-    Route::get('/games', [GameController::class, 'index']);
-    Route::post('/games', [GameController::class, 'store']);
-    Route::get('/games/{id}', [GameController::class, 'show']);
-    Route::put('/games/{id}', [GameController::class, 'update']);
-    Route::delete('/games/{id}', [GameController::class, 'destroy']);
+    Route::get('/games', [AdminGameController::class, 'index']);
+    Route::post('/games', [AdminGameController::class, 'store']);
+    Route::get('/games/{id}', [AdminGameController::class, 'show']);
+    Route::put('/games/{id}', [AdminGameController::class, 'update']);
+    Route::delete('/games/{id}', [AdminGameController::class, 'destroy']);
 
     // PRODUCT
-    Route::get('/products', [ProductController::class, 'index']);
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::get('/products/{id}', [ProductController::class, 'show']);
-    Route::put('/products/{id}', [ProductController::class, 'update']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::get('/products', [AdminProductController::class, 'index']);
+    Route::post('/products', [AdminProductController::class, 'store']);
+    Route::get('/products/{id}', [AdminProductController::class, 'show']);
+    Route::put('/products/{id}', [AdminProductController::class, 'update']);
+    Route::delete('/products/{id}', [AdminProductController::class, 'destroy']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
 });
