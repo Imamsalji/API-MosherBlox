c7a9c7de3900027a21ea9b8b9c906ec2e042fa41
imamsalji <imamsalji7@gmail.com>
2026-04-09 22:32:23 +0700

    Migration: Bahan Membuat article

---
+++ database/migrations/2026_04_09_153147_create_article_meta_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('article_meta', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('article_id')->constrained()->cascadeOnDelete();
+            $table->string('meta_key');
+            $table->text('meta_value')->nullable();
+
+            $table->timestamps();
+
+            $table->index(['article_id', 'meta_key']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('article_meta');
+    }
+};
