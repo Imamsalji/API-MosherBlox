018bfdeaf4bf47094ce92e0ff10fa76ba8d90526
imamsalji <imamsalji7@gmail.com>
2026-04-10 21:29:58 +0700

    mvc: Model Article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
     use HasFactory;
+
+    //Status Article
+    const STATUS_DRAFT  = 'draft';
+    const STATUS_PUBLISHED = 'published';
+    const STATUS_ARCHIVED = 'archived';
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'excerpt',
+        'thumbnail',
+        'status',
+        'published_at',
+        'author_id'
+    ];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected $appends = ['thumbnail_url'];
+
+    public function getThumbnailUrlAttribute(): string
+    {
+        return Storage::url($this->thumbnail);
+    }
 }
