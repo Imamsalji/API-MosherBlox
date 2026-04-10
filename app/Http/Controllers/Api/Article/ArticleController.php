714b4a778b9033e54434452b01a893581ddfb079
imamsalji <imamsalji7@gmail.com>
2026-04-10 21:08:30 +0700

    add Article Model

---
+++ app/Http/Controllers/Api/Article/ArticleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Article;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()  {
+
+    }
+}
